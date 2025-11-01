1d507e9045b2f65339d4148efdb272ddc75329f5
Abdisa.dev <meetabdisa@gmail.com>
2025-11-02 00:17:37 +0300

    operator intro

--- JS/Introduction/JS/script.js
+++ JS/Introduction/JS/script.js
@@ -1,22 +1,60 @@
-console.log("Hello TNT Ethiopia Family");
+// console.log("Hello TNT Ethiopia Family");
 
-// // Declaration
-let carName = null;
-const firstName = "Abdisa";
-const lastName = `ketema`;
+// // // Declaration
+// let carName = null;
+// const firstName = "Abdisa";
+// const lastName = `ketema`;
 
-// // // Assignment/Initialization
+// // // // Assignment/Initialization
 
-// let test = "Hello fam";
+// // let test = "Hello fam";
 
-// // Redeclaration
-// var test = 30;
-// console.log(test);
-// // Reassignment
-// test = "reassigning";
+// // // Redeclaration
+// // var test = 30;
+// // console.log(test);
+// // // Reassignment
+// // test = "reassigning";
 
-console.log(carName);
-console.log(firstName);
-console.log(lastName);
+// console.log(carName);
+// console.log(firstName);
+// console.log(lastName);
 
-typeof(carName)
+// typeof(carName)
+
+
+// Operators: arithmetic operators and assignment operators
+// ***************************************************************
+
+// Assignment operator( = )
+// let x = 10;
+// console.log(x);
+
+// // Addition operator (+)
+// let x = 10;
+// let y = 5;
+// let z = x + y; //z = 15;
+// console.log(z);
+
+// // Subtraction operator (-)
+// let x = 10;
+// let y = 5;
+// let z = x - y; //z = 5;
+// console.log(z);
+
+// // multiplication operator (*)
+// let x = 10;
+// let y = 5;
+// let z = x * y; // z = 50;
+// console.log(z);
+
+// // division operator
+// let x = 10;
+// let y = 5;
+// let z = x / y; //z = 2;
+// console.log(z);
+
+// modulus (remainder) operator (%)
+// let x = 10;
+// let y = 5;
+// let z = x % y; // remainder is
+// console.log(z);
