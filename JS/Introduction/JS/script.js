4e5c5bbc910886bb2024c259068f9317a83c4cc4
Abdisa.dev <meetabdisa@gmail.com>
2025-11-14 23:47:09 +0300

    arrays

--- JS/Introduction/JS/script.js
+++ JS/Introduction/JS/script.js
@@ -21,7 +21,6 @@
 
 // typeof(carName)
 
-
 // Operators: arithmetic operators and assignment operators
 // ***************************************************************
 
@@ -58,3 +57,95 @@
 // let y = 5;
 // let z = x % y; // remainder is
 // console.log(z);
+
+// 1️⃣ Pre-Increment (++x)
+// let x = 5;
+// console.log(++x); // adds 1 first → prints 6
+// console.log(x);   // still 6
+
+// // another example
+// let y = 10;
+// let result1 = ++y; // y becomes 11 before being used
+// console.log(result1); // 11
+// console.log(y);       // 11
+
+// // 2️⃣ Post-Increment (x++)
+// // let y1 = 5;
+// // console.log(y1++); // prints 5 first, then adds 1
+// // console.log(y1);   // now it's 6
+
+// // another example
+// let z = 7;
+// let result2 = z++; // result2 gets 7, then z becomes 8
+// console.log(result2); // 7
+// console.log(z);       // 8
+
+// String operator (concatenation operator) (+)
+// let a = "Hello";
+// let b = "World";
+// let c = a + " " + b;
+// console.log(c)
+// console.log(a + b)
+
+// // Equal( == )
+// console.log(4 == 4)
+// console.log(4 == "4");
+// console.log("abebe" > "kebe");
+
+// // Strictly Equal (identical or triple equal) ( === )
+// console.log(4 === 4);
+// console.log(4 == "4");
+
+// // Not identical (not strictly equal) ( !== )
+// console.log(4 != "4");
+// console.log(4 !== "4");
+
+// // // Less than ( < )
+// console.log(4 < 5);
+// console.log(4 < 4);
+
+// // // Greater than or equal to ( >= )
+// console.log(4 >= 4);
+// console.log(4 >= 5);
+
+// // Less than or equal to ( <= )
+// console.log(4 <= 4);
+// console.log(4 <= 3);
+
+// console.log(Number("12"))
+
+// 2.5 - Operators: logical operators
+// // !
+// // &&  AND
+// // ||   OR
+
+// // !true = false
+// // !false =true
+// // true or true => true
+// // true or false => true
+// // false or true => true
+// // false or false => false
+
+// console.log(1 == 1 && 2 == 2 && 3 == 7)
+// console.log(1 == 1 || 3 == 2 || 3 == 7);
+// console.log(!(1 == 1 || 3 == 2))
+
+//Arrays
+
+// Creating (Declaring) an Array
+// let a = "banana";
+
+// let fruits = ["Banana", "Apple", "Mango", 15, true, 8, 9];
+// // console.log(fruits)
+// // console.log(fruits[2]);
+
+// // Accessing the Elements of an Array
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[6]);
+
+// // Changing array elements/values
+
+// fruits[2] = "Grapes";
+// console.log(fruits);
