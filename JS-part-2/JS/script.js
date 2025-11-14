9eb962dc5354ff91307bbe8c1fe79ee84159cd31
Abdisa.dev <meetabdisa@gmail.com>
2025-11-15 00:58:43 +0300

    function declaration definition and Invoke

--- JS-part-2/JS/script.js
+++ JS-part-2/JS/script.js
@@ -1,1 +1,20 @@
-alert("hello world");
+// alert('hello world')
+// How do we define/declare a function in JS
+
+// Function Declaration
+function greet() {
+  console.log("Hello");
+}
+
+// const greet = function(){
+//     console.log("hello")
+// }
+
+// Function Expression
+const greet2 = function () {
+  console.log("Hello from exp");
+};
+
+// Invoking/Calling a function
+greet();
+greet2();
