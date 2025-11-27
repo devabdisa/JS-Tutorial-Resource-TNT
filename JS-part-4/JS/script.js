978ea9a9f129b12d599cc91b5a67d26a980e20fd
Abdisa.dev <meetabdisa@gmail.com>
2025-11-27 22:16:31 +0300

    Introduction to OOP

---
+++ JS-part-4/JS/script.js
@@ -0,0 +1,53 @@
+// The object oriented data model
+
+// - What is Data model
+
+      // Refers to the way data is structured, organized, and manipulated within a software system.
+
+// - What is Object Oriented Data Model?
+
+       // Data is represented and structured as an object.
+
+// Is a way of modeling real world objects as a software objects.
+
+// - Objects - are the things we are trying to define to the computer.
+
+// - Sometimes OOP refered as class based programming
+// - Classes - are a template for creating objects.
+
+// - Core components of objects
+
+// Attribute or Properties - are characteristics of the thing we are describing.
+// Method/function(action) - are actions that can be performed on objects.
+
+// The object oriented data model
+
+
+
+
+
+
+
+
+
+// - Analogy: A Car
+
+// 1. Attributes (Properties):
+
+// Color: The color of the car (e.g., red, blue).
+
+// Make: The manufacturer of the car (e.g., Toyota, Ford).
+
+// Model: The specific model of the car (e.g., Camry, F-150).
+
+// Year: The year the car was made (e.g., 2020).
+
+// 2. Methods (Actions):
+
+// Start Engine: A function that starts the car's engine.
+
+// Drive: A function that allows the car to move forward.
+
+// Stop: A function that stops the car.
+
+// - The car represents an object. It has specific attributes(properties) that describe it and methods that define what it can do.
