b741cc3e81eaa7cb5d7143a80b46203fa3ef6498
Abdisa.dev <meetabdisa@gmail.com>
2025-11-27 19:56:42 +0300

    for loop introduction

---
+++ JS-part-3/JS/script.js
@@ -0,0 +1,53 @@
+//  Decisions & loops (for loop)
+
+// - What are Loops in JS
+
+// Loops are control structures that allow a program to execute a block of code repeatedly based on a condition.
+
+// Loops offer a quick and easy way to do something repeatedly.
+
+// Loops are used for iterating over a collection of items or executing a set of instructions multiple times.
+
+// If a condition evaluates to true, the loop’s statement is executed.
+
+// When the condition evaluates to false, the loop stops.
+
+// Types of Loops in JS
+
+// for
+// for/in
+// for/of
+// while
+// do/while
+
+// JS for loop
+
+// loops through a block of code a number of times
+
+// - for loop Syntax
+
+// text
+
+// for (initialization; condition; update) {
+//         // statements (code block to be executed)
+// }
+
+
+
+// - initialization
+
+// Executed (one time) before the execution of the code block.
+
+// Used to initialize a variable
+
+// - condition
+
+// Defines the condition for executing the code block.
+
+// The code inside the for loop is executed as long as this condition is true.
+
+// - post/final-expression
+
+// Executed (every time) after the code block has been executed.
+
+// Updates the variable we initialized
