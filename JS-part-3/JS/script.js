5d5b7c45bc42a940659fc242fbddaf55dc912e92
Abdisa.dev <meetabdisa@gmail.com>
2025-11-27 21:27:27 +0300

    update

--- JS-part-3/JS/script.js
+++ JS-part-3/JS/script.js
@@ -32,8 +32,6 @@
 //         // statements (code block to be executed)
 // }
 
-
-
 // - initialization
 
 // Executed (one time) before the execution of the code block.
@@ -51,3 +49,101 @@
 // Executed (every time) after the code block has been executed.
 
 // Updates the variable we initialized
+
+// Examples of Decision Loops (For Loop)
+
+// A. Simple counter Loop
+// 0 - 2
+// for (let i = 0; i <= 2; i++) {
+//   console.log(i);
+// }
+
+// Analogy #1: Counting Down to New Year
+// ****************************
+// Analogy: Think of a countdown timer for New Year's Eve, where you want to count down the last ten seconds before midnight.
+
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// console.log("Happy new Year 💥🎇");
+
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// console.log("Happy New Year 🎇");
+
+// B. Iterate over array elements
+
+// Log elements of an array without using for Loop
+// let numbers = [1, 2, 3, 4, 5]
+
+// console.log(numbers[0])
+// console.log(numbers[1])
+// numbers.length
+
+// console.log(numbers.length);
+
+// using for Loop
+// for(let i =0;i < numbers.length;i++){
+//     console.log(numbers[i]);
+// }
+
+// for(let i = 0; i < numbers.length; i++){
+//     console.log(numbers[i])
+// }
+
+// let i = 0;
+// while (i < 3) {
+//   console.log(i); // prints 0, 1, 2
+//   i++;
+// }
+
+
+
+
+// break statement
+// used to exit a loop immediately, regardless of the loop's condition.
+
+
+
+// continue statement
+// skips the current iteration and moves to the next iteration of the loop.
+
+
+
+
+
+
+
+
+
+// C. Break and continue
+
+// Break
+
+// Find number 10 from arrays of numbers
+
+// let numbers = [1, 3, 6, 10, 12];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] == 10) {
+//     console.log(numbers[i]);
+//     break;
+//   }
+// }
+
+
+
+// Continue
+// Log every elements of an array except number 10
+
+// let numbers = [1, 3, 6, 10, 12,15,7,21];
+
+// for(let i = 0; i < numbers.length; i++){
+//     if(numbers[i] == 10){
+//         continue;
+//     }
+//     console.log(numbers[i]);
+// }
