f761de36bac834d065cb488aae6a83931968c566
Abdisa.dev <meetabdisa@gmail.com>
2025-12-11 23:03:23 +0300

    Dom intro

--- JS-part-5/JS/script.js
+++ JS-part-5/JS/script.js
@@ -1,3 +1,6 @@
-console.log(document.body);
-document.body.style.backgroundColor = "red";
+// console.log(document.body);
+// document.body.style.backgroundColor = "red";
 // alert("hello class")
+
+// console.dir(document.body.children[0].textContent);
+// let test = (document.body.children[0].textContent = "Test");
