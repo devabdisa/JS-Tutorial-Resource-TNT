5de0a8a7eccc820c592e3ccfe8096f2e7c3d323a
Abdisa.dev <meetabdisa@gmail.com>
2025-12-12 20:56:47 +0300

    DOM intro

--- JS-part-5/JS/script.js
+++ JS-part-5/JS/script.js
@@ -1,8 +1,17 @@
-console.dir(document);
-console.log(document.body.children);
-console.log(document.body.childNodes);
+// console.dir(document);
+// console.log(document.body.children);
+// console.log(document.body.childNodes);
 // document.body.style.backgroundColor = "red";
 // alert("hello class")
-
 // console.dir(document.body.children[0].textContent);
 // let test = (document.body.children[0].textContent = "Test");
+
+
+
+// DOM manipulation: introduction
+
+// Manipulating DOM involves:
+
+// 1.Finding/selecting the element we want to work with
+// 2.Altering the text or attributes of that element
+// 3.Event listening
